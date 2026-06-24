8dfb25510d7ba85baf298311509878a850b3c497
555hehe555 <123888406+555hehe555@users.noreply.github.com>
2026-06-24 15:33:29 +0300

    some edit styles

--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -2,7 +2,7 @@
 import styles from "./Header.module.scss";
 
 import UserCart from "../../UserCart/UserCart.tsx";
-// import BasicBtn from "../../Button/Button.tsx";
+import Button from "../../Button/Button.tsx";
 import Input from "../../Input/Input";
 
 export default function Header() {
@@ -13,6 +13,19 @@ export default function Header() {
       </div>
 
       <div className={styles.left}>
+        <div className={styles.containerLinks}>
+          <li className={styles.link}>
+            <Button size="sm" variant="primary">
+              Login
+            </Button>
+          </li>
+          <li className={styles.link}>
+            <Button size="sm" variant="primary">
+              Registration
+            </Button>
+          </li>
+        </div>
+
         <div className={styles.containerSeaech}>
           <Input type="text" size="md" placeholder="Search" maxLength={100} />
         </div>
